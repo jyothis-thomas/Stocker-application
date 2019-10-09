538b23e05ab1d123528e39726e967d8859b7cc63
jyothis-thomas <jyothisth@gmail.com>
2019-10-09 17:31:09 +0530

    Turned off debug

--- stocker/settings.py
+++ stocker/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vn!br$o4n8v$n3ruo($+hru9fl0!dhph91id_1#n+u9inytx!h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+DEBUG = False
+ALLOWED_HOSTS = ["stocker-application.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
